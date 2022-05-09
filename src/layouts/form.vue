4b464a4340b8e32c51cff2063e2b17fd3d6359bd
James Sinkala <xinnks@gmail.com>
2022-05-09 15:45:01 +0300

    feat: Add form wrapping layout

---
+++ src/layouts/form.vue
@@ -0,0 +1,12 @@
+<script setup>
+import notification from './../components/notification.vue';
+</script>
+
+<template>
+  <div class="p-12 w-full h-screen flex flex-col justify-center">
+    <div class="w-full">
+      <notification></notification>
+    </div>
+    <slot name="form"></slot>
+  </div>
+</template>
