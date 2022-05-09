77071f3dfcf383e1531a8cc58aad4f99430c9c10
James Sinkala <xinnks@gmail.com>
2022-05-09 15:45:56 +0300

    feat: Add page views wrapping layout

---
+++ src/layouts/page.vue
@@ -0,0 +1,14 @@
+<script setup>
+import notification from './../components/notification.vue';
+import navigation from './../components/navigation.vue';
+</script>
+
+<template>
+  <navigation></navigation>
+  <div class="m-0 p-8 h-screen w-full">
+    <div class="m-2 flex flex-col max-w-xl mx-auto space-y-8">
+      <notification></notification>
+      <slot name="page"></slot>
+    </div>
+  </div>
+</template>
