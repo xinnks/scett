a54f8a9f4cd0e3072587a438cac162421d18f9d9
James Sinkala <xinnks@gmail.com>
2022-05-12 20:23:48 +0300

    feat(src/layouts/page.vue): Add loading component to page component

--- src/layouts/page.vue
+++ src/layouts/page.vue
@@ -1,6 +1,7 @@
 <script setup>
 import notification from './../components/notification.vue';
 import navigation from './../components/navigation.vue';
+import loading from './../components/loading.vue';
 </script>
 
 <template>
@@ -8,6 +9,7 @@ import navigation from './../components/navigation.vue';
   <div class="m-0 p-8 h-screen w-full">
     <div class="m-2 flex flex-col max-w-xl mx-auto space-y-8">
       <notification></notification>
+      <loading></loading>
       <slot name="page"></slot>
     </div>
   </div>
