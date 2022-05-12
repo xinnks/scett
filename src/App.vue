9415cf1b4b36a80032df79c42e77d9821834a906
James Sinkala <xinnks@gmail.com>
2022-05-12 21:16:40 +0300

    feat(src/App.vue): Add router-view; change wrapper styling

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,7 @@ watch(checkAuth, (val) => {
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <router-view/>
 </template>
 
 <style>
@@ -28,8 +27,8 @@ watch(checkAuth, (val) => {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
 }
 </style>
