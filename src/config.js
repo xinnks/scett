a1bcf0092bd233f2b387634415d3a785111515e2
James Sinkala <xinnks@gmail.com>
2022-05-12 20:12:36 +0300

    feat: Add app level variables

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const Vars = {
+  endpoint : import.meta.env.VITE_APP_ENDPOINT,
+  project: import.meta.env.VITE_APP_PROJECT,
+  threadsCollection : import.meta.env.VITE_APP_THREADS_COLLECTION_ID,
+  twitterInfoCollection : import.meta.env.VITE_APP_TWITTER_INFO_COLLECTION_ID,
+  authEndpoint : import.meta.env.VITE_APP_AUTH_ENDPOINT,
+  twitterOauthFunction : import.meta.env.VITE_APP_TW_OAUTH_FUNCTION
+}
